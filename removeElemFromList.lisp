17a838f01019c1e63d9c56adf9c750f2697815e9
sundeepblue <sundeepblue@gmail.com>
2013-07-08 22:17:29 -0400

    Update removeElemFromList.lisp

--- removeElemFromList.lisp
+++ removeElemFromList.lisp
@@ -1,13 +1,19 @@
-;;; Write a function that takes a list and an element, and returns the original list with the first occurrence of the element removed.
+;;; remove the first occurrence of an element from a list
 
 ;;; A ==> (a b c c d)
 ;;; (remu A 'a) ==> (b c c d)
 ;;; (remu A 'c) ==> (a b c d)
 
-(defun remu (lis elem)
+(defun remu1 (lis elem)
  (if (null lis)
   (nil)
   (if (equal (first lis) elem) 
    (rest lis)
-   (cons (first lis) (remu (rest lis) elem)))))
+   (cons (first lis) (remu1 (rest lis) elem)))))
 
+;;; The above code has a concise form as follows:
+
+(defun remu2 (lis elem)
+  (cond ((null lis) nil)
+ 	((equal (first lis) elem) (rest lis))
+		(t (cons (first lis) (remu2 (rest lis) elem)))))
