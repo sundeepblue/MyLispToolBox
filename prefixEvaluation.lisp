3d4598b5053bc9904250de2775e3e92ddca36ec9
sundeepblue <sundeepblue@gmail.com>
2013-07-08 23:10:21 -0400

    Create prefixEvaluation.lisp

---
+++ prefixEvaluation.lisp
@@ -0,0 +1,15 @@
+;;; given an expression in prefix representation, evaluate its value
+
+;;; (evalu '(* (+ 2 3) (- 4 1))) ==> -15
+;;; (evalu '(+ (- 8 5) (* 2 (+ 4 2)))) ==> 15
+
+(defun evalu (expr)
+  (cond ((null expr) nil)
+  	(t (cond ((numberp expr) expr)
+			 	 ((equal (first expr) '*) 
+				  (* (evalu (second expr)) (evalu (third expr))))
+				 ((equal (first expr) '+)
+				  (+ (evalu (second expr)) (evalu (third expr))))
+				 ((equal (first expr) '-)
+				  (- (evalu (second expr)) (evalu (third expr))))))))
+
