7fd6f2dd68488ed1d93cc1cc4ac0643200ce950f
sundeepblue <sundeepblue@gmail.com>
2013-07-08 22:10:48 -0400

    add two test cases

--- removeElemFromList.lisp
+++ removeElemFromList.lisp
@@ -1,5 +1,9 @@
 ;;; Write a function that takes a list and an element, and returns the original list with the first occurrence of the element removed.
 
+;;; A ==> (a b c c d)
+;;; (remu A 'a) ==> (b c c d)
+;;; (remu A 'c) ==> (a b c d)
+
 (defun remu (lis elem)
  (if (null lis)
   (nil)
